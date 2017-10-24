adc7cbe2e468dddd4237bbbfa04b5c3ee8e85532
Swizec Teller <swizec@swizec.com>
2017-10-24 09:50:43 -0700

    Add google analytics

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,11 +3,17 @@ import PropTypes from "prop-types";
 import Link from "gatsby-link";
 import Helmet from "react-helmet";
 
+import ga from "react-google-analytics";
+
 import "bootstrap/dist/css/bootstrap.css";
 import "../css/bootstrap-override.css";
 import "../css/bootstrap-custom-utils.css";
 import "../css/salesbury-lilac.css";
 import "./index.css";
+
+const GAInitiailizer = ga.Initializer;
+ga("create", "UA-1464315-1", "auto");
+ga("send", "pageview");
 
 const TemplateWrapper = ({ children }) => (
     <div className="cheatsheet">
@@ -16,7 +22,10 @@ const TemplateWrapper = ({ children }) => (
             <script async src="https://gumroad.com/js/gumroad.js" />
 
             <meta name="author" content="Swizec Teller" />
-            <meta name="description" content="Interactive ES6 cheatsheet." />
+            <meta
+                name="description"
+                content="SWIZ IS TURNING 30 SO EVERYTHING IS $30."
+            />
             <meta
                 name="viewport"
                 content="width=device-width, initial-scale=1.0"
@@ -37,18 +46,23 @@ const TemplateWrapper = ({ children }) => (
                 content="Get everything Swiz has ever made. Learn React, Redux, MobX, D3v4, ES6+ and more for $30 on his birthday. 1 day only."
             />
 
-            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:card" content="summary_large_image" />
             <meta name="twitter:site" content="@swizec" />
             <meta name="twitter:creator" content="@swizec" />
-            <meta name="twitter:title" content="Interactive ES6 cheatsheet" />
+            <meta
+                name="twitter:title"
+                content="SWIZ IS TURNING 30 SO EVERYTHING IS $30"
+            />
+            <meta
+                name="twitter:description"
+                content="Get everything Swiz has ever made. Learn React, Redux, MobX, D3v4, ES6+ and more for $30 on his birthday. 1 day only"
+            />
             <meta
                 name="twitter:image"
                 content="https://instagram.fsnc1-1.fna.fbcdn.net/t51.2885-15/e35/22500001_137654163533198_4910693116064825344_n.jpg"
             />
-            <meta
-                name="twitter:description"
-                content="Get everything Swiz has ever made. Learn React, Redux, MobX, D3v4, ES6+ and more for $30 on his birthday. 1 day only."
-            />
+
+            <GAInitiailizer />
         </Helmet>
         {children()}
     </div>
